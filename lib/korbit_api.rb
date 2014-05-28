fe4e59cdb909211a6333f73406b9e1bc6990d8ae
Charlie Moseley <charlie@rofl.io>
2014-05-29 08:20:07 +0900

    Adds missing requires.

--- lib/korbit_api.rb
+++ lib/korbit_api.rb
@@ -1,8 +1,18 @@
+# TODO: Organize requires
 require 'typhoeus'
 require 'hashie'
 require 'addressable/uri'
 require 'korbit_api/client'
 require 'korbit_api/utils'
+require 'korbit_api/coins'
+require 'korbit_api/crypto_address'
+require 'korbit_api/fiat_address'
+require 'korbit_api/order'
+require 'korbit_api/orders'
+require 'korbit_api/transfer'
+require 'korbit_api/transfer_status'
+require 'korbit_api/user'
+require 'korbit_api/wallet'
 
 module KorbitAPI
 end
